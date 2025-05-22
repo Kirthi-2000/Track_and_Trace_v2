3c39b73c6c724fcad4ad1746ce431639ef7dadac
Kirthi-2000 <135959708+Kirthi-2000@users.noreply.github.com>
2025-05-22 17:46:22 +0530

    Review changes in Bulk Upload 22.05.2025

---
+++ adminportal/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('baseview/',BaseView.as_view(),name="baseview"),
+    path('base/',IndexView.as_view(),name="base"),
+    path('BulkUpload/',BulkUpload.as_view(),name="BulkUpload"),
+    
+    path('Table/',Table.as_view(),name="Table"),
+]
